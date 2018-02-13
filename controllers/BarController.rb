65a92b56d0e497026d9c08eb8bf6fbb9e4ed37df
Ryne Laster <ryne.laster@gmail.com>
2018-02-13 08:56:49 -0600

    Controller routes completed

--- controllers/BarController.rb
+++ controllers/BarController.rb
@@ -1,5 +1,38 @@
 class BarController < ApplicationController
+	
+	get '/' do
+		@bars = Bar.all
+		@bars.to_json
+	end
 
+	get '/:id/' do
+		@bar = Bar.find params[:id]
+		stuff = @bar.itemorders
+		stuff.to_json
+	end 
+
+	post '/' do
+		@bar = Bar.new
+		@bar.name = params[:name]
+		@bar.save
+		@bar.to_json
+	end
+
+	put '/:id' do
+		@bar = Bar.find params[:id]
+		@bar.name = params[:name]
+		@bar.save
+		@bar.to_json
+	end
+
+	delete '/:id' do
+		@bar = Bar.find params[:id]
+		bname = @bar.name
+		@bar.delete
+		{
+			message: "Bar #{bname} deleted"
+		}.to_json
+	end
 
 
 
--- controllers/RestaurantController.rb
+++ controllers/RestaurantController.rb
@@ -1,6 +1,32 @@
 class RestaurantController < ApplicationController
 
+	get '/' do
+		@restaurants = Restaurant.all
+		@restaurants.to_json
+	end
 
+	post '/' do
+		@restaurant = Restaurant.new
+		@restaurant.name = params[:name]
+		@restaurant.save
+		@restaurant.to_json
+	end
+
+	put '/:id' do
+		@restaurant = Restaurant.find params[:id]
+		@restaurant.name = params[:name]
+		@restaurant.save
+		@restaurant.to_json
+	end
+
+	delete '/:id' do
+		@restaurant = Restaurant.find params[:id]
+		restaurant = @restaurant.name
+		@restaurant.delete
+		{
+			message: "Restaurant #{restaurant} deleted"
+		}.to_json
+	end
 
 
 
